db1e5ceae2098d49c8b19138a039b408542adb15
disarra <disarra02@mail.ru>
2023-10-01 10:13:31 +0300

    Основа для отображения на экран

---
+++ draw/draw.c
@@ -0,0 +1,47 @@
+#include <X11/Xlib.h>
+#include <stdio.h>
+#include <err.h>
+#include "draw.h"
+
+static Display* dpy;
+static int scr;
+static Window root;
+static Window window;
+static GC graphicsContext;
+
+#define POSX 500
+#define POSY 500
+#define WIDTH 500
+#define HEIGHT 500
+#define BORDER 15
+
+void open_window()
+{
+	Window win;
+	XEvent ev;
+	if ((dpy = XOpenDisplay(NULL)) == NULL)
+		err(1, "Can not open display");
+	scr = DefaultScreen(dpy);
+	root = RootWindow(dpy, scr);
+
+	win = XCreateSimpleWindow(dpy, root, POSX, POSY, WIDTH, HEIGHT, BORDER, BlackPixel(dpy, scr), WhitePixel(dpy, scr));
+	XMapWindow(dpy, win);
+	window = win;
+	graphicsContext  = XCreateGC(dpy, window, 0, 0);
+}
+
+void close_window()
+{
+	XUnmapWindow(dpy, window);
+	XDestroyWindow(dpy, window);
+	XCloseDisplay(dpy);
+}
+
+
+
+void draw_point(int x, int y, short red, short green, short blue)
+{
+	unsigned long color = (red << 16) || (green << 16) || blue;
+	XSetForeground(dpy, graphicsContext, color);
+	XDrawPoint(dpy, window, graphicsContext, x, y);
+}
---
+++ draw/draw.h
@@ -0,0 +1,11 @@
+#ifndef __DRAW__
+#define __DRAW__
+
+void open_window();
+
+void close_window();
+
+void draw_point(int x, int y, short red, short green, short blue);
+
+#endif
+
